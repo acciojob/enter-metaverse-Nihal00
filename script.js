33cecf43b6f85a0a840200a770733d1ee74af174
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-16 10:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const p = document.getElementById('status');
+const btn = document.getElementById('enterBtn');
+
+function change() {
+	p.innerText = 'Entered Metaverse';
+}
+
+btn.addEventListener('click', change);
